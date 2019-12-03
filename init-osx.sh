888512a1fff0fc28a891758b33b298a492607e5e
Brian Devins-Suresh <badevins@gmail.com>
2019-12-03 15:12:18 -0500

    Update init-osx.sh

--- init-osx.sh
+++ init-osx.sh
@@ -1,16 +1,38 @@
 #!/bin/sh
 
-xcode-select --install
+if ! xcode-select -p > /dev/null 2>&1; then
+  xcode-select --install
+fi
 
-while ! xcode-select -p 1>/dev/null 2>/dev/null
+while ! xcode-select -p > /dev/null 2>&1
 do
   sleep 10
 done
 
-if ! xcode-select -p 1>/dev/null 2>/dev/null; then
+if ! xcode-select -p > /dev/null 2>&1; then
   echo "xcode command line tools not installed"
   exit 1
 fi
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! brew commands > /dev/null 2>&1; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 brew cask install iterm2
+
+#ZSH
+brew install zsh
+sudo sh -c "echo \"$(which zsh)\" >> /etc/shells"
+chsh -s "$(which zsh)"
+brew install antibody
+
+#RCM
+brew tap thoughtbot/formulae
+brew install rcm
+
+#Dotfiles
+mkdir -p ${HOME}/.local/opt
+git clone git@github.com:devinsba/dotfiles.git "${HOME}/.local/opt/dotfiles"
+echo "DOTFILES_DIRS=\"${HOME}/.local/opt/dotfiles\"" > ~/.rcrc
+echo "TAGS=\"$(uname)\"" >> ~/.rcrc
+
+rcup -vf
